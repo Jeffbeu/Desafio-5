0b5a5b01e0f5c8d1c3bbf825acf203897076b24d
jeffbeu <douglas-oliveira12@hotmail.com>
2021-08-19 15:39:41 -0300

    upload readme

---
+++ script.js
@@ -0,0 +1,12 @@
+var w = 0;
+$(function() {
+ "use strict";
+    /* -------- preloader ------- */
+    $(window).on("load", function() {
+        $('#preloader').delay(2000).fadeOut(500);
+    });
+    /* -------- preloader End ------- */
+
+
+});
+
